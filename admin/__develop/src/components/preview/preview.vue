bdd9cd86596f5690f3ee20e6f14d0a03acb330de
linqian <1432667757@qq.com>
2018-04-28 11:00:49 +0800

    阅览作业

--- admin/__develop/src/components/preview/preview.vue
+++ admin/__develop/src/components/preview/preview.vue
@@ -1,119 +1,45 @@
-<template>
-  <div class="box-preview wrapper">
-    <h4>{{info.name}}</h4>
-    <p class="text-center">
-      <span>{{info.user_name}}</span> -
-      <span>{{info.class_name}}</span>
-    </p>
-    <div class="box-iframe">
-      <canvas id="canvas"></canvas>
-    </div>
-    <div class="info">
-      <div class="box-action">
-        <i class="el-icon-download"></i>
-        <a :href="w.download_url" target="_blank" style="color:#60c5ff;">下载</a>
-        <span>文件大小:{{w.size}}</span>
-      </div>
-      <p class="box-info"><span>作品描述：</span><span>{{w.intro || '无'}}</span></p>
-    </div>
-  </div>
-</template>
-<script>
-// 预览
-import PDFJS from 'vue-pdf'
-export default {
-	name: 'preview',
-	// props: ['parentData'],
-	data() {
-		return {
-			info: {
-					name: '作品名称',
-			user_name: '李想',
-			class_name: '泡小西区-五年级一班',
-			work_detail: {
-				url: 'http://localhost:8081/work_file/7ca63ec615d34f30c7ea32dd1a2024ce.docx',
-				format: 'docx',
-				size: '15M',
-				intro: '作品描述：微课是互联网+时代的一种越来越普遍的信息技术教学工具。在课前、课中、课后都有广泛的应用前景'
-			}
-			}
-		}
-	},
-	computed: {
-		w() {
-			return this.info.work_detail;
-		}
-	},
-	created() {
-        
-		    //   var url = 'http://localhost:8081/upload/e5266cda72e3920f828083a540351349.docx';
-         
-        //    PDFJS.getDocument(url).then(function getPdf(pdf) {
-        //            pdf.getPage(2).then(function getPage(page) {
-        //                        var scale = 1;
-        //                        var viewport = page.getViewport(scale);
-        //                        var canvas = document.getElementById('canvas');
-        //                        var context = canvas.getContext('2d');
-        //                        canvas.height = viewport.height;
-        //                        canvas.width = viewport.width;
-        //                        var renderContext = {
-        //                            canvasContext: context,
-        //                                viewport: viewport
-        //                       };
-        //                   page.render(renderContext);
-        //                 });
-        // });
 
-		let data = {
-			name: '作品名称',
-			user_name: '李想',
-			class_name: '泡小西区-五年级一班',
-			work_detail: {
-				url: 'http://localhost:8081/upload/e5266cda72e3920f828083a540351349.docx',
-				format: 'pdf',
-				size: '15M',
-				intro: '作品描述：微课是互联网+时代的一种越来越普遍的信息技术教学工具。在课前、课中、课后都有广泛的应用前景'
-			}
-		}
-
-		Object.assign(this.info,  JSON.parse(this.$route.query.data));
-
-	}
-}
-</script>
-<style scoped>
-  .wrapper {
-    margin: 0 auto;
-    max-width: 800px;
-    background-color: #f1f1f1;
-    padding: 40px;
+<template>  
+  <div id="container">  
+  </div>  
+</template>  
+<script>  
+import pdf from '@/components/pdf'  
+  
+export default {  
+  data () {  
+    return {  
+    }  
+  },  
+  methods: {  
+  
+  }  ,
+  mounted() {  
+      // this.$showPDF("http://localhost:8081/work_file/8fc5fdc3a5906e964e2e01b85f972463.pdf")
   }
 
-  .box-iframe {
-    height: 400px;
-  }
-
-  iframe {
-    width: 100%;
-    height: 100%;
-  }
-
-  h4 {
-    font-size: 16px;
-    font-weight: bold;
-    line-height: 1.5;
-    text-align: center;
-  }
-
-  .box-action,
-  h4,
-  p {
-    margin: 10px 0;
-  }
-
-  .info {
-    background-color: #fff;
-    padding: 10px;
-  }
-
-</style>
+}  
+</script>  
+  
+<style scoped>  
+#container {  
+  background-color: rgba(0,0,0,0.75);  
+  position:fixed;  
+  width: 100%;  
+  height: 100%;  
+  top: 0;  
+  left: 0;  
+  text-align: center;  
+  padding: 5px;  
+}  
+  
+.pdf-page {  
+  
+}  
+  
+.foot {  
+  position: fixed;  
+  transform: translate(-50%,0);  
+  left: 50%;  
+}  
+</style>  
